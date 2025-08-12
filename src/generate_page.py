5e76fcef4ffdeabfd97e4bd66ee6ba3944688561
andynesse <ann.nesse@stud.uis.no>
2025-08-12 20:27:24 +0200

    Add: Generated site

--- src/generate_page.py
+++ src/generate_page.py
@@ -2,7 +2,7 @@ import os
 from markdown_to_html_node import markdown_to_html_node
 from extract_markdown import extract_title
 
-def generate_page(from_path, template_path, dest_path):
+def generate_page(from_path, template_path, dest_path, basepath):
     print(f"Generating page from {from_path} to {dest_path} using {template_path}")
 
     markdown = ""
@@ -17,20 +17,19 @@ def generate_page(from_path, template_path, dest_path):
         title = "untitled"
     content = markdown_to_html_node(markdown).to_html()
 
-    template = template.replace("{{ Title }}", title)
-    template = template.replace("{{ Content }}", content)
+    template = template.replace("{{ Title }}", title).replace("{{ Content }}", content).replace('href="/', f'href="{basepath}')
 
     with open(dest_path, "w", encoding="utf-8") as file:
         written = file.write(template)
         if written != len(template):
             raise Exception(f"did not write to {dest_path} correctly. Expected written chars: {len(template)}, Actual: {written}")
         
-def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path, basepath):
     dirlist = os.listdir(dir_path_content)
     for item in dirlist:
         path = os.path.join(dir_path_content, item)
         if os.path.isfile(path):
-            generate_page(path, template_path, os.path.join(dest_dir_path, item.replace(".md", ".html")))
+            generate_page(path, template_path, os.path.join(dest_dir_path, item.replace(".md", ".html")), basepath)
             continue
         os.mkdir(os.path.join(dest_dir_path, item))
-        generate_pages_recursive(path, template_path, os.path.join(dest_dir_path, item))
+        generate_pages_recursive(path, template_path, os.path.join(dest_dir_path, item), basepath)
--- src/main.py
+++ src/main.py
@@ -1,27 +1,37 @@
 import os
 import shutil
+import sys
+
 from generate_page import generate_pages_recursive
 def main():
-    public_path = "./public"
+    basepath = sys.argv[0]
+    if basepath == "":
+        basepath = "/"
+    
+    docs_path = "./docs"
     static_path = "./static"
+    content_path = "./content"
 
-    clear_content(public_path)
+    clear_content(docs_path)
 
-    print(f"Copying files from '{static_path}' to '{public_path}':")
-    copy_contents(static_path, public_path)
+    print(f"Copying files from '{static_path}' to '{docs_path}':")
+    copy_contents(static_path, docs_path)
     print("Copy complete!")
-    generate_pages_recursive("./content", "./template.html", "./public")
+    generate_pages_recursive(content_path, "./template.html", docs_path, basepath)
 
 def clear_content(path):
     print(f"Clearing '{path}' dir:")
-    for item in os.listdir(path):
-        item_path = os.path.join(path, item)
-        if os.path.isfile(item_path):
-            os.remove(item_path)
-            print("-- Removed", item_path)
-        else:
-            shutil.rmtree(item_path)
-            print("-- Removed", item_path, "and its content")
+    if os.path.exists(path):
+        for item in os.listdir(path):
+            item_path = os.path.join(path, item)
+            if os.path.isfile(item_path):
+                os.remove(item_path)
+                print("-- Removed", item_path)
+            else:
+                shutil.rmtree(item_path)
+                print("-- Removed", item_path, "and its content")
+    else:
+        os.mkdir(path)
     print("Directory cleared!\n")
 
 def copy_contents(from_path, to_path):        
